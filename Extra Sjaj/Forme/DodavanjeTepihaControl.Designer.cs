c58023f6ea371ba4a7e3878ba380e06cc70e6eaa
Ilhan <ilhandeveloper@gmail.com>
2018-11-01 22:11:26 +0100

    promjena dizajna na dodavanje tepiha kontroli

--- Extra Sjaj/Forme/DodavanjeTepihaControl.Designer.cs
+++ Extra Sjaj/Forme/DodavanjeTepihaControl.Designer.cs
@@ -44,6 +44,8 @@
             this.labelPlaceno = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -51,11 +53,11 @@
             // labelRacun
             // 
             this.labelRacun.AutoSize = true;
-            this.labelRacun.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRacun.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelRacun.ForeColor = System.Drawing.Color.Black;
-            this.labelRacun.Location = new System.Drawing.Point(3, 142);
+            this.labelRacun.Location = new System.Drawing.Point(69, 171);
             this.labelRacun.Name = "labelRacun";
-            this.labelRacun.Size = new System.Drawing.Size(83, 25);
+            this.labelRacun.Size = new System.Drawing.Size(135, 41);
             this.labelRacun.TabIndex = 2;
             this.labelRacun.Text = "Račun:";
             // 
@@ -73,8 +75,11 @@
             // btnDodajTepih
             // 
             this.btnDodajTepih.BackColor = System.Drawing.Color.Transparent;
-            this.btnDodajTepih.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnDodajTepih.Location = new System.Drawing.Point(104, 296);
+            this.btnDodajTepih.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnDodajTepih.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDodajTepih.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDodajTepih.ForeColor = System.Drawing.Color.White;
+            this.btnDodajTepih.Location = new System.Drawing.Point(67, 77);
             this.btnDodajTepih.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnDodajTepih.Name = "btnDodajTepih";
             this.btnDodajTepih.Size = new System.Drawing.Size(209, 89);
@@ -86,31 +91,31 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(199, 260);
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(158, 30);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(20, 24);
+            this.label4.Size = new System.Drawing.Size(28, 33);
             this.label4.TabIndex = 15;
             this.label4.Text = "x";
             // 
             // txtBoxDuzina
             // 
             this.txtBoxDuzina.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtBoxDuzina.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxDuzina.Location = new System.Drawing.Point(227, 255);
+            this.txtBoxDuzina.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxDuzina.Location = new System.Drawing.Point(212, 26);
             this.txtBoxDuzina.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtBoxDuzina.Name = "txtBoxDuzina";
-            this.txtBoxDuzina.Size = new System.Drawing.Size(49, 33);
+            this.txtBoxDuzina.Size = new System.Drawing.Size(64, 43);
             this.txtBoxDuzina.TabIndex = 13;
             // 
             // txtBoxSirina
             // 
             this.txtBoxSirina.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtBoxSirina.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxSirina.Location = new System.Drawing.Point(137, 255);
+            this.txtBoxSirina.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxSirina.Location = new System.Drawing.Point(67, 26);
             this.txtBoxSirina.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtBoxSirina.Name = "txtBoxSirina";
-            this.txtBoxSirina.Size = new System.Drawing.Size(48, 33);
+            this.txtBoxSirina.Size = new System.Drawing.Size(61, 43);
             this.txtBoxSirina.TabIndex = 12;
             // 
             // btnNaplati
@@ -120,7 +125,7 @@
             this.btnNaplati.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnNaplati.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNaplati.ForeColor = System.Drawing.Color.Black;
-            this.btnNaplati.Location = new System.Drawing.Point(104, 62);
+            this.btnNaplati.Location = new System.Drawing.Point(467, 159);
             this.btnNaplati.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnNaplati.Name = "btnNaplati";
             this.btnNaplati.Size = new System.Drawing.Size(195, 76);
@@ -147,7 +152,7 @@
             "1.4",
             "1.5",
             "1.6"});
-            this.comboBoxCijena.Location = new System.Drawing.Point(351, 38);
+            this.comboBoxCijena.Location = new System.Drawing.Point(614, 20);
             this.comboBoxCijena.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBoxCijena.Name = "comboBoxCijena";
             this.comboBoxCijena.Size = new System.Drawing.Size(48, 28);
@@ -158,29 +163,27 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
-            this.panel2.Controls.Add(this.listBoxTepiha);
-            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.txtBoxDuzina);
             this.panel2.Controls.Add(this.btnDodajTepih);
             this.panel2.Controls.Add(this.txtBoxSirina);
             this.panel2.Controls.Add(this.label4);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(593, 0);
+            this.panel2.Location = new System.Drawing.Point(0, 410);
             this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(426, 638);
+            this.panel2.Size = new System.Drawing.Size(342, 193);
             this.panel2.TabIndex = 19;
             // 
             // listBoxTepiha
             // 
             this.listBoxTepiha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.listBoxTepiha.Dock = System.Windows.Forms.DockStyle.Top;
             this.listBoxTepiha.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBoxTepiha.FormattingEnabled = true;
             this.listBoxTepiha.ItemHeight = 28;
             this.listBoxTepiha.Location = new System.Drawing.Point(0, 0);
             this.listBoxTepiha.Name = "listBoxTepiha";
-            this.listBoxTepiha.Size = new System.Drawing.Size(426, 228);
+            this.listBoxTepiha.Size = new System.Drawing.Size(342, 424);
             this.listBoxTepiha.TabIndex = 22;
             this.listBoxTepiha.DoubleClick += new System.EventHandler(this.listBoxTepiha_DoubleClick);
             // 
@@ -194,18 +197,17 @@
             this.panel1.Controls.Add(this.labelImePrezime);
             this.panel1.Controls.Add(this.labelRacun);
             this.panel1.Controls.Add(this.labelPlaceno);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel1.ForeColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(0, 440);
+            this.panel1.Location = new System.Drawing.Point(343, 317);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(426, 198);
+            this.panel1.Size = new System.Drawing.Size(676, 272);
             this.panel1.TabIndex = 21;
             // 
             // textBoxNaplate
             // 
             this.textBoxNaplate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBoxNaplate.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxNaplate.Location = new System.Drawing.Point(318, 86);
+            this.textBoxNaplate.Location = new System.Drawing.Point(594, 118);
             this.textBoxNaplate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBoxNaplate.Name = "textBoxNaplate";
             this.textBoxNaplate.Size = new System.Drawing.Size(67, 33);
@@ -216,7 +218,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(289, 41);
+            this.label3.Location = new System.Drawing.Point(552, 23);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 18);
             this.label3.TabIndex = 20;
@@ -227,7 +229,7 @@
             this.labelPlaceno.AutoSize = true;
             this.labelPlaceno.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelPlaceno.ForeColor = System.Drawing.Color.Black;
-            this.labelPlaceno.Location = new System.Drawing.Point(3, 48);
+            this.labelPlaceno.Location = new System.Drawing.Point(5, 40);
             this.labelPlaceno.Name = "labelPlaceno";
             this.labelPlaceno.Size = new System.Drawing.Size(73, 18);
             this.labelPlaceno.TabIndex = 11;
@@ -240,7 +242,7 @@
             this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnBack.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBack.ForeColor = System.Drawing.Color.Black;
-            this.btnBack.Location = new System.Drawing.Point(505, 4);
+            this.btnBack.Location = new System.Drawing.Point(937, 0);
             this.btnBack.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(82, 76);
@@ -252,18 +254,42 @@
             // panel3
             // 
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Location = new System.Drawing.Point(27, 87);
+            this.panel3.Location = new System.Drawing.Point(342, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(548, 436);
+            this.panel3.Size = new System.Drawing.Size(589, 315);
             this.panel3.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(78, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Širina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(221, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Dužina";
             // 
             // DodavanjeTepihaControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
+            this.Controls.Add(this.listBoxTepiha);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.panel3);
             this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "DodavanjeTepihaControl";
@@ -293,5 +319,7 @@
         private System.Windows.Forms.TextBox textBoxNaplate;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
