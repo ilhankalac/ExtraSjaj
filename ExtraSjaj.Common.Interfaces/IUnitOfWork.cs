564a6e139861c45c93b3308fc848be3cf357e924
Ilhan <ilhandeveloper@gmail.com>
2018-11-07 22:20:33 +0100

    repo pattern definisan za musteriju

---
+++ ExtraSjaj.Common.Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraSjaj.Common.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IMusterijaRepository Musterije { get; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+
+    }
+}
---
+++ ExtraSjaj.DAL.RepoPattern/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using ExtraSjaj.Common.Interfaces;
+using ExtraSjaj.Modeli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraSjaj.DAL.RepoPattern
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ModelContext _context;
+        public IMusterijaRepository Musterije { get; private set; }
+        public UnitOfWork(ModelContext context)
+        {
+            _context = context;
+            Musterije = new MusterijaRepository(_context);
+
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
