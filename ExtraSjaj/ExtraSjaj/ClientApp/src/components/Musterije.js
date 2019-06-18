fd7475518a0ee8c172d1823eb408ab00dba7ffea
Ilhan <ilhandeveloper@gmail.com>
2019-06-18 19:32:00 +0200

    fetching data musterijes component - react

---
+++ ExtraSjaj/ExtraSjaj/ClientApp/src/components/Musterije.js
@@ -0,0 +1,57 @@
+﻿import React, { Component } from 'react';
+
+export class Musterije extends Component {
+
+    static displayName = Musterije.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { musterijeNiz: [], loading: true };
+
+        fetch('api/Musterijas')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ musterijeNiz: data, loading: false });
+            });
+    }
+
+    static renderMusterijasTable(musterijeNiz) {
+        return (
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>Ime</th>
+                        <th>Prezime</th>
+                        <th>Broj telefona</th>
+                        <th>Adresa</th>
+                        <th>Vrijeme kreiranja mušterije</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {musterijeNiz.map(item =>
+                        <tr key={item.Id}>
+                            <td>{item.ime}</td>
+                            <td>{item.prezime}</td>
+                            <td>{item.brojTelefona}</td>
+                            <td>{item.adresa}</td>
+                            <td>{item.vrijemeKreiranjaMusterije}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Musterije.renderMusterijasTable(this.state.musterijeNiz);
+
+        return (
+            <div>
+                <h1>Lista mušterija</h1>
+                {contents}
+            </div>
+        );
+    }
+}
---
+++ ExtraSjaj/ExtraSjaj/Controllers/MusterijasController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ExtraSjaj.Common.Models;
+using ExtraSjaj.DAL.Context;
+
+namespace ExtraSjaj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusterijasController : ControllerBase
+    {
+        private readonly ExtraSjajContext _context;
+
+        public MusterijasController(ExtraSjajContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Musterijas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Musterija>>> GetMusterije()
+        {
+            return await _context.Musterije.ToListAsync();
+        }
+
+        // GET: api/Musterijas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Musterija>> GetMusterija(int id)
+        {
+            var musterija = await _context.Musterije.FindAsync(id);
+
+            if (musterija == null)
+            {
+                return NotFound();
+            }
+
+            return musterija;
+        }
+
+        // PUT: api/Musterijas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMusterija(int id, Musterija musterija)
+        {
+            if (id != musterija.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(musterija).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MusterijaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Musterijas
+        [HttpPost]
+        public async Task<ActionResult<Musterija>> PostMusterija(Musterija musterija)
+        {
+            _context.Musterije.Add(musterija);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMusterija", new { id = musterija.Id }, musterija);
+        }
+
+        // DELETE: api/Musterijas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Musterija>> DeleteMusterija(int id)
+        {
+            var musterija = await _context.Musterije.FindAsync(id);
+            if (musterija == null)
+            {
+                return NotFound();
+            }
+
+            _context.Musterije.Remove(musterija);
+            await _context.SaveChangesAsync();
+
+            return musterija;
+        }
+
+        private bool MusterijaExists(int id)
+        {
+            return _context.Musterije.Any(e => e.Id == id);
+        }
+    }
+}
