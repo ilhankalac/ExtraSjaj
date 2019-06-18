df77e919093806f4a6d2d718d7821e6ce5a1ab48
Ilhan <ilhandeveloper@gmail.com>
2019-06-18 17:33:23 +0200

    adding repo pattern and unit of work in project

---
+++ ExtraSjaj/ExtraSjaj.DAL.RepoPattern/MusterijaRepository.cs
@@ -0,0 +1,24 @@
+﻿using ExtraSjaj.Common.Interfaces;
+using ExtraSjaj.Common.Models;
+using ExtraSjaj.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtraSjaj.DAL.RepoPattern
+{
+    public class MusterijaRepository : Repository<Musterija>, IMusterijaRepository
+    {
+        protected readonly ExtraSjajContext _context;
+
+        public MusterijaRepository(ExtraSjajContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public ExtraSjajContext context
+        {
+            get { return context as ExtraSjajContext; }
+        }
+    }
+}
---
+++ ExtraSjaj/ExtraSjaj.DAL.RepoPattern/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using ExtraSjaj.Common.Interfaces;
+using ExtraSjaj.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraSjaj.DAL.RepoPattern
+{
+    public class UnitOfWork : IUnitOfWork
+    { 
+        private readonly ExtraSjajContext _context;
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
