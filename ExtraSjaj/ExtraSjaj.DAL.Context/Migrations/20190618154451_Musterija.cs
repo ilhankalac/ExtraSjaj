397d462af358f9c8f843842a68423414cb6e65e0
Ilhan <ilhandeveloper@gmail.com>
2019-06-18 17:46:41 +0200

    migration musterija done

---
+++ ExtraSjaj/ExtraSjaj.DAL.Context/Migrations/20190618154451_Musterija.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExtraSjaj.DAL.Context.Migrations
+{
+    public partial class Musterija : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Musterije",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Ime = table.Column<string>(nullable: true),
+                    Prezime = table.Column<string>(nullable: true),
+                    BrojTelefona = table.Column<string>(nullable: true),
+                    Adresa = table.Column<string>(nullable: true),
+                    VrijemeKreiranjaMusterije = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musterije", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Musterije");
+        }
+    }
+}
