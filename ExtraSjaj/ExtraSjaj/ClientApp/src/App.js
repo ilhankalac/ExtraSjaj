251a776739bd132f7d44695efab349fe7d0bbdc2
Ilhan <ilhandeveloper@gmail.com>
2019-06-18 19:32:00 +0200

    fetching data musterijes component - react

--- ExtraSjaj/ExtraSjaj/ClientApp/src/App.js
+++ ExtraSjaj/ExtraSjaj/ClientApp/src/App.js
@@ -4,6 +4,7 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { Musterije } from './components/Musterije';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -14,6 +15,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Route path='/Musterije' component={Musterije} />
       </Layout>
     );
   }
