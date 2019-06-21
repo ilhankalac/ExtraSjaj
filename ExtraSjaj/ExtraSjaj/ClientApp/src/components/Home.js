952927ed3d2d8ef7fec80aca7d9cf9d313e64c31
Ilhan <ilhandeveloper@gmail.com>
2019-06-21 16:06:13 +0200

    imported reactstrap and bootstrap

--- ExtraSjaj/ExtraSjaj/ClientApp/src/components/Home.js
+++ ExtraSjaj/ExtraSjaj/ClientApp/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Button} from 'reactstrap'
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -8,7 +9,7 @@ export class Home extends Component {
       <div>
         <h1>Hello, world!</h1>
         <p>Welcome to your new single-page application, built with:</p>
-        <ul>
+            <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
           <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
--- ExtraSjaj/ExtraSjaj/ClientApp/src/index.js
+++ ExtraSjaj/ExtraSjaj/ClientApp/src/index.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
