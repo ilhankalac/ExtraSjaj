07bc23b0cecf8568f99a9d4cd3f650e9308ef74f
Ilhan <ilhandeveloper@gmail.com>
2019-06-21 18:22:36 +0200

    redirecting to create and scuffed design for creating customer

--- ExtraSjaj/ExtraSjaj/ClientApp/src/App.js
+++ ExtraSjaj/ExtraSjaj/ClientApp/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Route, Router, browserHistory } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { axios } from 'axios';
 import { Musterije } from './components/Musterije';
+import { CreateMusterija } from './components/Musterija/CreateMusterija';
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-        <Route path='/Musterije' component={Musterije} />
+        <Layout>
+            <Router history={browserHistory}>
+                <Route exact path='/' component={Home} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetch-data' component={FetchData} />
+                <Route path='/Musterije' component={Musterije} />
+                <Route path='/CreateMusterija' component={CreateMusterija} />
+            </Router>
       </Layout>
     );
   }
---
+++ ExtraSjaj/ExtraSjaj/ClientApp/src/components/Musterija/CreateMusterija.js
@@ -0,0 +1,47 @@
+﻿import React, { Component } from 'react';
+import { Button, Table, InputGroup, InputGroupAddon, InputGroupText, Input, Label, Row, Col } from 'reactstrap';
+import axios from 'axios';
+
+
+export class CreateMusterija extends Component {
+
+    static renderMusterijasTable() {
+        return (
+            <Row>
+                <Col sm={{ size: 6, offset: 1 }}>
+                    <Label className="mr-2" for="ime"> Ime </Label>
+                    <Input className="mr-2" type="text" id="Ime" placeholder="Ime" />
+                </Col>
+                <Col  sm={{ size: 6, offset: 1 }}>
+                    <Label className="mr-2" for="prezime"> Prezime </Label>
+                    <Input className="mr-2" type="text" id="Ime" placeholder="Prezime" />
+                </Col>
+                <Col sm={{ size: 6, offset: 1 }}>
+                    <Label className="mr-2" for="prezime"> Broj telefona </Label>
+                    <Input className="mr-2" type="text" id="Ime" placeholder="Broj telefona" />
+                </Col>
+                <Col sm={{ size: 6, offset: 1 }}>
+                    <Label className="mr-2" for="prezime"> Adresa </Label>
+                    <Input className="mr-2" type="text" id="Ime" placeholder="Adresa" />
+                </Col>
+                <Col sm={{ size: 6, offset: 1 }}>
+                    <Button className="mt-4" color="success"> Kreiraj </Button>
+                </Col>
+            </Row>
+        );
+    }
+
+    render() {
+        
+        let contents = CreateMusterija.renderMusterijasTable();
+
+        return (
+            <div>
+                <h1>Lista mušterija</h1>
+                {contents}
+            </div>
+        );
+    }
+
+
+}
