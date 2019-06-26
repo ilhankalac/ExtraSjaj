c58ecb0906ba37013ef31af4c5b2d21af25148e8
Ilhan <ilhandeveloper@gmail.com>
2019-06-26 22:42:11 +0200

    side navigation drawer responsive

--- ExtraSjaj/ExtraSjaj/ClientApp/src/components/Layout.js
+++ ExtraSjaj/ExtraSjaj/ClientApp/src/components/Layout.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import Drawer from '../Drawer';
+
 
 export class Layout extends Component {
   static displayName = Layout.name;
 
   render () {
     return (
-      <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
+        <div>
+            <Drawer props={this.props.children} />
       </div>
     );
   }
